52a535e5be0a9dc52893a0c3c37e2296328468a9
dkg2 <dkg2mail@gmail.com>
2026-09-13 18:50:16 +0530

    Fix 413 Payload Too Large by increasing express.json limit to 50mb

--- aurix-backend/server.js
+++ aurix-backend/server.js
@@ -41,7 +41,8 @@ app.use(cors({
   },
   credentials: true
 }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.get('/', (req, res) => {
     res.send(`
